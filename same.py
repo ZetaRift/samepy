20469eb1aed7240b792320f1bd5d717d0ba61eb7
ZetaRift <zetarift@gmail.com>
2017-03-30 21:33:25 -0400

    Create same.py

---
+++ same.py
@@ -0,0 +1,38 @@
+import wave, struct, math
+
+#constants
+preamble = '10101011101010111010101110101011101010111010101110101011101010111010101110101011101010111010101110101011101010111010101110101011'
+footer = '01001110010011100100111001001110'
+highbit = 2083.3
+lowbit = 1562.5
+bitlength = 0.00192
+
+
+def binencode(arg):
+ c = ''
+ for s in arg:
+  c+=''.join(str(ord(s) >> i & 1) for i in range(8)[::-1])
+ return c
+
+
+if __name__ == '__main__':
+ exampleheader = input("Enter message: ")  
+ encodedheader = preamble + binencode(exampleheader)
+ wavef = wave.open('same.wav', 'w')
+ wavef.setnchannels(1)
+ wavef.setsampwidth(2)
+ wavef.setframerate(44100)
+ for s in encodedheader:
+  sb = 0.0
+  if s == "1":
+   sb = highbit
+  else:
+   sb = lowbit
+  i = 0
+  while i < bitlength*44100:
+   value = int(16383.0*math.sin((2*math.pi)*(float(i/44100))*sb))
+   data = struct.pack('<h', value)
+   wavef.writeframesraw(data)
+   i+=1
+
+ wavef.close()
